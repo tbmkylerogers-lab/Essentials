1b7339f7a543c3b7f15a1e10abc8548ed4185593
tbmkylerogers-lab <tbmkylerogers@gmail.com>
2026-04-07 13:34:22 -0600

    Reapply "inclass 12"
    This reverts commit d0a60f7dadea0a1e1a09c7dd985098bfdf48813b.

---
+++ DAGV1700/AdventureGame/Assets/myscripts/npcflipper.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCFlipper : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // Only flip if the agent is actually moving horizontally
+        float xVel = agent.velocity.x;
+
+        if (Mathf.Abs(xVel) > 0.01f)
+        {
+            // Flip when moving left
+            spriteRenderer.flipX = xVel < 0f;
+        }
+    }
+}
